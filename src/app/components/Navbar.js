828eedfcc4c490e8d356000749d5b23ad5611d70
Toqeer Afzal <48534849+ToqeerAfzal@users.noreply.github.com>
2024-12-17 11:42:15 +0500

    navbar managed

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -92,6 +92,10 @@ const Navbar = () => {
     return (
         <nav className="fixed top-0 w-full z-10 bg-white shadow-md">
             <div className="container mx-auto px-6 py-4 flex justify-between items-center">
+            <div className="container mx-auto px-6 py-4 flex justify-between items-center">
+                <div className="text-2xl font-bold text-black">Realm</div>
+                <div class="w-[24px] h-[24px] bg-[#45473C] rounded-full"></div>
+            </div>
                 <div className="text-2xl font-bold text-black">Realm</div>
                 <div class="w-[24px] h-[24px] bg-[#45473C] rounded-full"></div>
             </div>
