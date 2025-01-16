27917ce08f67be3249995db030a7e2d70854979c
Toqeer Afzal <48534849+ToqeerAfzal@users.noreply.github.com>
2025-01-16 20:47:50 +0500

    layout fixed

--- src/app/mine/layout.js
+++ src/app/mine/layout.js
@@ -25,11 +25,6 @@ export default function RootLayout({ children }) {
       >
         {children}
       </body>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
     </html>
   );
 }
