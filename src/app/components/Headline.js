c532f471a5a206d317ff3f5a0f142c881219bf03
Arij Idrees <56958669+arijkhan94@users.noreply.github.com>
2025-07-22 03:26:23 +0500

    put together all code

--- src/app/components/Headline.js
+++ src/app/components/Headline.js
@@ -46,6 +46,16 @@ const Headline = () => {
                 We craft tailored websites that elevate architectural spaces into digital experiences.
             </h1>
         </section>
+         <section className="h-screen flex justify-center items-center bg-gradient-to-b from-white to-black">
+            <h1 className="text-7xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
+                We craft tailored websites that elevate architectural spaces into digital experiences.
+            </h1>
+        </section>
+         <section className="h-screen flex justify-center items-center bg-gradient-to-b from-white to-black">
+            <h1 className="text-7xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
+                We craft tailored websites that elevate architectural spaces into digital experiences.
+            </h1>
+        </section>
     );
 };
 
