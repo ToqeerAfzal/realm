290a476e104ac70cacdd469243ff9f67b94ec484
Arij Idrees <56958669+arijkhan94@users.noreply.github.com>
2025-07-27 17:21:41 +0500

    New navbar included

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -300,24 +300,12 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <nav className="fixed top-0 w-full z-10 bg-white shadow-md">
-            <div className="container mx-auto px-6 py-4 flex justify-between items-center">
-                <div className="text-2xl font-bold text-black">Realm</div>
-                <div class="w-[24px] h-[24px] bg-[#45473C] rounded-full"></div>
-            </div>
-        </nav>
-        <nav className="fixed top-0 w-full z-10 bg-white shadow-md">
-        <div className="container mx-auto px-6 py-4 flex justify-between items-center">
-            <div className="text-2xl font-bold text-black">Realm</div>
-            <div class="w-[24px] h-[24px] bg-[#45473C] rounded-full"></div>
-        </div>
-    </nav>
-    <nav className="fixed top-0 w-full z-10 bg-white shadow-md">
-    <div className="container mx-auto px-6 py-4 flex justify-between items-center">
-        <div className="text-2xl font-bold text-black">Realm</div>
-        <div class="w-[24px] h-[24px] bg-[#45473C] rounded-full"></div>
-    </div>
-</nav>
+        <Navbar>
+            <menubar>first menu</menubar>
+            <menubar>second menu</menubar>
+            <menubar>third menu</menubar>
+            <menubar>fourth menu</menubar>
+        </Navbar>
     );
 };
 
