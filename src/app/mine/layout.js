b2017e70ffb40ec5d3bf7f5cc2adbf6f6230af04
Toqeer Afzal <48534849+ToqeerAfzal@users.noreply.github.com>
2025-08-04 23:28:34 +0500

    child content scrollbar added

--- src/app/mine/layout.js
+++ src/app/mine/layout.js
@@ -22,6 +22,7 @@ export default function RootLayout({ children }) {
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
           {children}
+          child content added
         </body>
       </body>
       <body
